a3012e62bfd1a2c08e72f9eaa3b67a5f701a289b
iguanakun <yw16027@gmail.com>
2023-09-30 16:15:46 +0900

    ユーザ登録のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
+ VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+ VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+ VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+
+ validates :password, format: { with: VALID_PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers'}
+
  validates :nickname, presence: true
- validates :first_name, presence: true
- validates :last_name, presence: true
- validates :first_name_kana, presence: true
- validates :last_name_kana, presence: true
+ validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: ' is invalid. Input full-width characters'}
+ validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: ' is invalid. Input full-width characters'}
+ validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: ' is invalid. Input full-width characters'}
+ validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: ' is invalid. Input full-width characters'}
  validates :birthday, presence: true
 
  devise :database_authenticatable, :registerable,
