50adf72fe2d4e6f55e81da6b2e8df0cc0774ce99
iguanakun <yw16027@gmail.com>
2023-09-30 18:31:56 +0900

    rubocop対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
- VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
- VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
- VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
- validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'}
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
 
- validates :nickname, presence: true
- validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters'}
- validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters'}
- validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: 'is invalid. Input full-width katakana characters'}
- validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: 'is invalid. Input full-width katakana characters'}
- validates :birthday, presence: true
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters' }
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters' }
+  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
+  validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
+  validates :birthday, presence: true
 
- devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,15 +4,14 @@ FactoryBot.define do
       person { Gimei.name }
     end
 
-    nickname {Faker::Name.last_name}
-    email {Faker::Internet.email}
-    password {Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1, min_numeric: 1)}
-    password_confirmation {password}
-    first_name {person.first.kanji}
-    last_name {person.last.kanji}
-    first_name_kana {person.first.katakana}
-    last_name_kana {person.last.katakana}
-    birthday {Faker::Date.backward }
-
+    nickname { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    first_name { person.first.kanji }
+    last_name { person.last.kanji }
+    first_name_kana { person.first.katakana }
+    last_name_kana { person.last.katakana }
+    birthday { Faker::Date.backward }
   end
 end
