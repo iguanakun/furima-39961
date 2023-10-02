8a19960683f32d49e9ed0b6e228bcc93a1fc0164
iguanakun <yw16027@gmail.com>
2023-10-02 15:28:45 +0900

    アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_cost
+  belongs_to :delivery_day
+  belongs_to :prefecture
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many: items
 end
