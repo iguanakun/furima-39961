7d81cc0e2f41669e922241029d4904dc712614d1
iguanakun <yw16027@gmail.com>
2023-10-05 14:38:45 +0900

    クレジットカード決済の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,14 +3,18 @@ class OrdersController < ApplicationController
 
   def index
     @order_shipping = OrderShipping.new
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
   end
 
   def create
+    binding.pry
     @order_shipping = OrderShipping.new(order_params)
     if @order_shipping.valid?
+      pay_item
       @order_shipping.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -19,10 +23,19 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_shipping).permit(:postal_code, :prefecture_id, :municipalities, :house_number, 
-                   :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+                   :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,14 +1,13 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number, :item_id, :user_id, :token
 
 
 
   with_options presence: true do
-    validates :user_id, :item_id
+    validates :user_id, :item_id, :municipalities, :house_number, :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :municipalities, :house_number
     validates :phone_number, length: { in: 10..11 }, numericality: { only_integer: true, message: 'is invalid. Input only number' }
   end
 
